b50c2e390d746e1abf9df3dcff5463d075e328d4
cuongpct109 <81698029+cuongpct109@users.noreply.github.com>
2022-08-30 14:08:56 +0700

    Delete shutdown-process.sh

--- shutdown-process.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "\n"
-rm -rf ~/Downloads/skipid-infra 
-cd ~/Downloads 
-echo "cloning skipid-infra source code from github/cuongpct109/skipid-infra"
-echo "==============================="
-git clone https://github.com/cuongpct109/skipid-infra.git > /dev/null 2>&1 
-sh ~/Downloads/skipid-infra/shutdown.sh
