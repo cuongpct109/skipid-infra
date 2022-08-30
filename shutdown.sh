85828b1625229400b69d374bbb4e2f2931a3dbf3
cuongpct109 <81698029+cuongpct109@users.noreply.github.com>
2022-08-30 11:38:58 +0700

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,7 +1,53 @@
 #!/bin/sh
 
-kill -9 $(lsof -i tcp:3000 | awk '{ print $2 }' | uniq | sed '2q;d')
+# Kill processes which using 3000 port
 
-kill -9 $(lsof -i tcp:8080 | awk '{ print $2 }' | uniq | sed '2q;d')
+for i in $(lsof -i:3000 | awk '{print $2}' | uniq | tail -n+2 |)
+do
+kill -9 $i 
+done
 
-cd ~/Documents/dbs && sudo docker-compose down
+# Kill processes which using 8080 port
+for i in $(lsof -i:8080 | awk '{print $2}' | uniq | tail -n+2 | tr '\n' ' ')
+do
+kill -9 $i 
+done
+
+# kill mysql containers 
+mysql=$(sudo docker ps | grep "mysql" | awk '{ print $1 }' | uniq)
+
+if [ -z "$mysql" ]
+then
+    echo "docker mysql container is not running"
+else
+    for i in $mysql
+    do
+        sudo docker stop $i > /dev/null && echo "mysql container with ID = $i has stopped"
+    done
+fi
+
+# kill mongodb containers
+mongo=$(sudo docker ps | grep "mongo" | awk '{ print $1 }' | uniq)
+
+if [ -z "$mongo" ]
+then
+    echo "mongo container is not running"
+else
+    for i in $mongo
+    do
+        sudo docker stop $i > /dev/null && echo "mongo container with ID = $i has stopped"
+    done
+fi
+
+# kill redis containers
+redis=$(sudo docker ps | grep "redis" | awk '{ print $1 }' | uniq)
+
+if [ -z "$redis" ]
+then
+    echo "redis container is not running"
+else
+    for i in $redis
+    do
+        sudo docker stop $i > /dev/null && echo "redis container with ID = $i has stopped"
+    done
+fi
