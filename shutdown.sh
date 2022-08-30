14ae77e43993ef9ad8f480f0cbf2e98575eabe6a
cuongpct109 <81698029+cuongpct109@users.noreply.github.com>
2022-08-30 10:01:53 +0700

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 port_3000=${ lsof -i tcp:3000 | awk '{ print $2 }' | uniq | sed '2q;d' } 
-kill -9 port3000
+kill -9 $port3000
 port_8080=${ lsof -i tcp:8080 | awk '{ print $2 }' | uniq | sed '2q;d' }
-kill -9 port8080
+kill -9 $port8080
 cd ~/Documents/dbs && sudo docker-compose down
