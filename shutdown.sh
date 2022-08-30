09f6576047333b8d42f4b2565d3d22e773279134
cuongpct109 <81698029+cuongpct109@users.noreply.github.com>
2022-08-30 11:56:16 +0700

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -4,11 +4,16 @@
 port_3000=$(lsof -i:3000 | awk '{print $2}' | uniq | tail -n+2)
 if [ -z $port_3000 ]
 then 
+    echo "==============================="
     echo "nothing is running on port 3000"
+    echo "==============================="
 else
     for i in $port_3000
     do
-        kill -9 $i > /dev/null && echo "killing process (ID=$i) with port 3000"
+        kill -9 $i > /dev/null 
+        echo "==============================="
+        echo "killing process (ID=$i) with port 3000"
+        echo "==============================="
     done
 fi
 
@@ -16,11 +21,16 @@ fi
 port_8080=$(lsof -i:8080 | awk '{print $2}' | uniq | tail -n+2)
 if [ -z $port_8080 ]
 then 
+    echo "==============================="
     echo "nothing is running on port 8080"
+    echo "==============================="
 else
     for i in $port_8080
     do
-        kill -9 $i > /dev/null && echo "killing process (ID=$i) with port 8080"
+        kill -9 $i > /dev/null 
+        echo "==============================="
+        echo "killing process (ID=$i) with port 8080"
+        echo "==============================="
     done
 fi
 
@@ -29,11 +39,17 @@ mysql=$(sudo docker ps | grep "mysql" | awk '{ print $1 }' | uniq)
 
 if [ -z "$mysql" ]
 then
+    echo "==============================="
     echo "mysql container is not running"
+    echo "==============================="
 else
     for i in $mysql
     do
-        sudo docker stop $i > /dev/null && echo "mysql container with ID = $i has stopped"
+        
+        sudo docker stop $i > /dev/null
+        echo "==============================="
+        echo "mysql container with ID = $i has stopped"
+        echo "==============================="
     done
 fi
 
@@ -42,11 +58,16 @@ mongo=$(sudo docker ps | grep "mongo" | awk '{ print $1 }' | uniq)
 
 if [ -z "$mongo" ]
 then
+    echo "==============================="
     echo "mongo container is not running"
+    echo "==============================="
 else
     for i in $mongo
     do
-        sudo docker stop $i > /dev/null && echo "mongo container with ID = $i has stopped"
+        sudo docker stop $i > /dev/null 
+        echo "==============================="
+        echo "mongo container with ID = $i has stopped"
+        echo "==============================="
     done
 fi
 
@@ -55,10 +76,15 @@ redis=$(sudo docker ps | grep "redis" | awk '{ print $1 }' | uniq)
 
 if [ -z "$redis" ]
 then
+    echo "==============================="
     echo "redis container is not running"
+    echo "==============================="
 else
     for i in $redis
     do
-        sudo docker stop $i > /dev/null && echo "redis container with ID = $i has stopped"
+        sudo docker stop $i > /dev/null 
+        echo "==============================="
+        echo "redis container with ID = $i has stopped"
+        echo "==============================="
     done
 fi
