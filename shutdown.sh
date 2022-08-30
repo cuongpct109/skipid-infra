eb4f4d614110c3fa230af02ce4a1a0acb5187843
cuongpct109 <81698029+cuongpct109@users.noreply.github.com>
2022-08-30 09:53:35 +0700

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 kill -9 ${lsof -i tcp:3000 | awk '{ print $2 }' | uniq | sed '2q;d' }
 kill -9 ${lsof -i tcp:8080 | awk '{ print $2 }' | uniq | sed '2q;d' }
 cd ~/Documents/dbs && sudo docker-compose down
