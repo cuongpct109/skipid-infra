d79685b80f6a9f4ef758648ebfaf7f4e0e7186ec
cuongpct109 <81698029+cuongpct109@users.noreply.github.com>
2022-08-30 10:03:12 +0700

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 port_3000=${ lsof -i tcp:3000 | awk '{ print $2 }' | uniq | sed '2q;d' } 
 kill -9 $port3000
 port_8080=${ lsof -i tcp:8080 | awk '{ print $2 }' | uniq | sed '2q;d' }
