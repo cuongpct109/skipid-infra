108d5d1bffe3a5ce6e4fbd28ef6cedb8d3ec3a07
cuongpct109 <81698029+cuongpct109@users.noreply.github.com>
2022-08-30 15:30:45 +0700

    Update skipid-dbs.sh

--- skipid-dbs.sh
+++ skipid-dbs.sh
@@ -1,6 +1,89 @@
 #!/bin/sh
-rm -rf ~/Documents/dbs ~/Downloads/dbs ~/Downloads/datastack ~/Downloads/datastack.zip 
-sudo apt update; sudo apt install -y wget && wget --load-cookies ~/Downloads/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies ~/Downloads/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1BodY1ghZjTjOgsrCeq9JARoS-kOA_ejZ' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1BodY1ghZjTjOgsrCeq9JARoS-kOA_ejZ" -O ~/Downloads/datastack.tar.gz && rm ~/Downloads/cookies.txt
-cd ~/Downloads && cd ~/Downloads && tar -xvf datastack.tar.gz && cd ~/Documents && git clone https://gitlab.com/ultorex/skipid/backend/dbs.git && rsync -a ~/Downloads/DataStack/ ~/Documents/dbs/
-sudo apt install -y docker && sudo apt install -y docker-compose && sudo systemctl enable docker.service && sudo systemctl enable containerd.service && sudo systemctl restart docker.service && sudo systemctl restart containerd.service && cd ~/Documents/dbs && sudo docker-compose up > /dev/null & 
-sudo apt autoremove; rm -rf ~/Downloads/skipid-infra ~/Downloads/DataStack ~/Downloads/datastack.tar.gz 
+
+# check and remove existing old folders
+if [ -d ~/Documents/dbs ] 
+then
+    rm -rf ~/Documents/dbs
+    echo "~/Documents/dbs removed"
+else
+    :
+fi
+
+if [ -d ~/Downloads/datastack ]    
+then
+    rm -rf ~/Downloads/datastack
+    echo "~/Downloads/datastack removed"
+else
+    :
+fi
+
+
+if [ -f ~/Downloads/datastack.tar.gz ]
+then
+    rm -rf ~/Downloads/datastack.tar.gz
+    echo "~/Downloads/datastack.tar.gz removed"
+else 
+    :
+fi
+
+
+# update and install necessary apps
+sudo apt update;
+
+# check and install wget and curl
+
+if [ -z "$(dpkg -l | grep -E '^ii' | grep wget)" ]
+then
+      sudo apt install -y wget
+else
+      echo "wget've already installed in this computer"
+fi
+
+if [ -z "$(dpkg -l | grep -E '^ii' | grep curl)" ]
+then
+      sudo apt install -y curl
+else
+      echo "curl've already installed in this computer"
+fi
+
+if [ -z "$(dpkg -l | grep -E '^ii' | grep docker)" ]
+then
+      sudo apt install -y docker
+else
+      echo "docker've already installed in this computer"
+fi
+
+if [ -z "$(dpkg -l | grep -E '^ii' | grep docker-compose)" ]
+then
+      sudo apt install -y docker-compose
+else
+      echo "docker-compose've already installed in this computer"
+fi
+
+if [ -z "$(ps aux | grep "docker" | grep -v "grep")" ]
+then
+      sudo systemctl enable docker.service
+      sudo systemctl restart docker.service
+else
+      echo "docker.service've already running in this computer"
+fi
+
+if [ -z "$(ps aux | grep "containerd" | grep -v "grep")" ]
+then
+      sudo systemctl enable containerd.service
+      sudo systemctl restart containerd.service
+else
+      echo "containerd.service've already running in this computer"
+fi
+
+# download datastack from https://drive.google.com/drive/u/0/folders/17i0jINA19m9PZ1u292Tht5lm5sZIWhcm, if the file change, just need to replace different file'ID
+wget --load-cookies ~/Downloads/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies ~/Downloads/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1BodY1ghZjTjOgsrCeq9JARoS-kOA_ejZ' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1BodY1ghZjTjOgsrCeq9JARoS-kOA_ejZ" -O ~/Downloads/datastack.tar.gz && rm ~/Downloads/cookies.txt
+
+# unzip datastack.tar.gz
+cd ~/Downloads && tar -xvf datastack.tar.gz && git clone https://gitlab.com/ultorex/skipid/backend/dbs.git ~/Documents/dbs && rsync -a ~/Downloads/DataStack/ ~/Documents/dbs/
+
+# run docker-compose and save lock to ~/Docu
+docker-compose -f ~/Documents/dbs/docker-compose.yml up > ~/Documents/dbs/docker.log 2>&1 &
+
+sudo apt autoremove; 
+rm -rf ~/Downloads/DataStack ~/Downloads/datastack.tar.gz 
