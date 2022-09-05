1f7c807f157fc75edfac714472019989640e27d3
cuongpct109 <81698029+cuongpct109@users.noreply.github.com>
2022-09-05 22:18:17 +0700

    Update staging-deploy.sh

--- staging-deploy.sh
+++ staging-deploy.sh
@@ -12,73 +12,9 @@ eval "$(curl https://raw.githubusercontent.com/cuongpct109/skipid-infra/main/pre
 
 sudo apt update;
 
-# Download apache-tomcat-8.0.53 and apache-ant-1.9.16
+# Installing apache-tomcat-8.0.53, apache-ant-1.9.16 and java1.8-272
 
-cd ~ && wget https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.53/bin/apache-tomcat-8.0.53.zip && unzip -o apache-tomcat-8.0.53.zip && rm apache-tomcat-8.0.53.zip && chmod +x ~/apache-tomcat-8.0.53/bin/catalina.sh ~/apache-tomcat-8.0.53/bin/startup.sh ~/apache-tomcat-8.0.53/bin/shutdown.sh
-cd ~ && wget https://dlcdn.apache.org//ant/binaries/apache-ant-1.9.16-bin.zip && unzip -o apache-ant-1.9.16-bin.zip && rm apache-ant-1.9.16-bin.zip && chmod +x ~/apache-ant-1.9.16/bin/ant ~/apache-ant-1.9.16/bin/antRun
-
-# Check current shell and configure JAVA if needed
-
-if [ "$(echo $0)" = "bash" ]; then
-    if [ -d ~/java1.8-272 ] 
-    then
-        rm -rf ~/java1.8-272
-        cd ~ && wget https://builds.openlogic.com/downloadJDK/openlogic-openjdk/8u272-b10/openlogic-openjdk-8u272-b10-linux-x64.tar.gz 
-        tar -xvf openlogic-openjdk-8u272-b10-linux-x64.tar.gz
-        rm -rf openlogic-openjdk-8u272-b10-linux-x64.tar.gz*
-        mv openlogic-openjdk-8u272-b10-linux-x64 java1.8-272 
-        echo "export JAVA_HOME=~/java1.8-272\nexport PATH=\$PATH:\$JAVA_HOME/bin\n" >> ~/.bashrc 
-        source ~/.bashrc
-    else
-        cd ~ && wget https://builds.openlogic.com/downloadJDK/openlogic-openjdk/8u272-b10/openlogic-openjdk-8u272-b10-linux-x64.tar.gz 
-        tar -xvf openlogic-openjdk-8u272-b10-linux-x64.tar.gz 
-        rm -rf openlogic-openjdk-8u272-b10-linux-x64.tar.gz* 
-        mv openlogic-openjdk-8u272-b10-linux-x64 java1.8-272
-        echo "export JAVA_HOME=~/java1.8-272\nexport PATH=\$PATH:\$JAVA_HOME/bin\n" >> ~/.bashrc
-        source ~/.bashrc
-    fi
-fi
-
-if [ "$(echo $0)" = "sh" ]; then
-    if [ -d ~/java1.8-272 ] 
-    then
-        rm -rf ~/java1.8-272
-        cd ~ && wget https://builds.openlogic.com/downloadJDK/openlogic-openjdk/8u272-b10/openlogic-openjdk-8u272-b10-linux-x64.tar.gz 
-        tar -xvf openlogic-openjdk-8u272-b10-linux-x64.tar.gz
-        rm -rf openlogic-openjdk-8u272-b10-linux-x64.tar.gz*
-        mv openlogic-openjdk-8u272-b10-linux-x64 java1.8-272 
-        echo "export JAVA_HOME=~/java1.8-272\nexport PATH=\$PATH:\$JAVA_HOME/bin\n" >> ~/.bashrc 
-        source ~/.bashrc
-    else
-        cd ~ && wget https://builds.openlogic.com/downloadJDK/openlogic-openjdk/8u272-b10/openlogic-openjdk-8u272-b10-linux-x64.tar.gz 
-        tar -xvf openlogic-openjdk-8u272-b10-linux-x64.tar.gz 
-        rm -rf openlogic-openjdk-8u272-b10-linux-x64.tar.gz*
-        mv openlogic-openjdk-8u272-b10-linux-x64 java1.8-272
-        echo "export JAVA_HOME=~/java1.8-272\nexport PATH=\$PATH:\$JAVA_HOME/bin\n" >> ~/.bashrc
-        source ~/.bashrc
-    fi
-fi
-
-if [ "$(echo $0)" = "zsh" ]; then
-    if [ -d ~/java1.8-272 ] 
-    then
-        rm -rf ~/java1.8-272
-        cd ~ && wget https://builds.openlogic.com/downloadJDK/openlogic-openjdk/8u272-b10/openlogic-openjdk-8u272-b10-linux-x64.tar.gz 
-        tar -xvf openlogic-openjdk-8u272-b10-linux-x64.tar.gz
-        rm -rf openlogic-openjdk-8u272-b10-linux-x64.tar.gz*
-        mv openlogic-openjdk-8u272-b10-linux-x64 java1.8-272 
-        echo "export JAVA_HOME=~/java1.8-272\nexport PATH=\$PATH:\$JAVA_HOME/bin\n" >> ~/.zshrc 
-        source ~/.zshrc
-    else
-        cd ~ && wget https://builds.openlogic.com/downloadJDK/openlogic-openjdk/8u272-b10/openlogic-openjdk-8u272-b10-linux-x64.tar.gz 
-        tar -xvf openlogic-openjdk-8u272-b10-linux-x64.tar.gz 
-        rm -rf openlogic-openjdk-8u272-b10-linux-x64.tar.gz*
-        mv openlogic-openjdk-8u272-b10-linux-x64 java1.8-272
-        echo "export JAVA_HOME=~/java1.8-272\nexport PATH=\$PATH:\$JAVA_HOME/bin\n" >> ~/.zshrc
-        source ~/.zshrc
-    fi
-fi
-
+eval "$(curl https://raw.githubusercontent.com/cuongpct109/skipid-infra/main/install-tomcat-ant-java.sh)"
 
 # Clone common and kyc-api source
 
