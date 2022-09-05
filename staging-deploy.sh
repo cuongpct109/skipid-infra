7d2c23bc9ecd0a08fed9caa7d3715ab13a63e2c3
cuongpct109 <81698029+cuongpct109@users.noreply.github.com>
2022-09-05 21:44:37 +0700

    Update staging-deploy.sh

--- staging-deploy.sh
+++ staging-deploy.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/zsh
 
 # Create .env file in ~/Documents/utilities/ folder
 
-echo "NODE_ENV=production\nREACT_APP_API_URI=https://staging-api.skipid.io\nREACT_APP_REACT_QUERY_DEVTOOLS=false\nREACT_APP_AUTH0_DOMAIN=auth.skipid.org\nREACT_APP_AUTH0_CLIENT_ID=rVgg324pRjwrtiF8FGOQYcsg4zKOyacn\nREACT_APP_AUTH0_CLIENT_AUDIENCE=https://skipid.net\nREACT_APP_AUTH0_CLIENT_AUDIENCE_MFA=https://dev-hvhnhg1u.us.auth0.com/mfa/\nREACT_APP_ULTOREX_URL=https://ultorex.org\nREACT_APP_SENTRY_DSN=https://865c108123e74465aa2bf6412b10b942@o1009406.ingest.sentry.io/6025507" > ~/Documents/utilities/.env
+eval "$(curl https://raw.githubusercontent.com/cuongpct109/skipid-infra/main/env-config.sh)"
 
 # Kill processes which using 3000 port
 
