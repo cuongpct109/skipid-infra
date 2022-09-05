5e3e2b3a2065b71c2f03ac0aa3d530a9be0ad3db
cuongpct109 <81698029+cuongpct109@users.noreply.github.com>
2022-09-05 12:01:36 +0000

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -2,29 +2,30 @@
 
 # Kill processes which using 3000 port
 port_3000=$(lsof -i:3000 | awk '{print $2}' | uniq | tail -n+2)
-if [ -z $port_3000 ]
-then 
+if [ -z $port_3000 ]             
+then
     echo "nothing is running on port 3000"
     echo "==============================="
 else
-    for i in $port_3000
+    for i in $(echo $port_3000 | sed "s/,/ /g")  
     do
-        kill -9 $i > /dev/null 2>&1
+        kill -9 $i 
         echo "killing process (ID=$i) with port 3000"
         echo "==============================="
     done
 fi
 
+
 # Kill processes which using 8080 port
 port_8080=$(lsof -i:8080 | awk '{print $2}' | uniq | tail -n+2)
-if [ -z $port_8080 ]
-then 
-    echo "nothing is running on port 8080"
+if [ -z $port_8080 ]             
+then
+    echo "nothing is running on port 3000"
     echo "==============================="
 else
-    for i in $port_8080
+    for i in $(echo $port_8080 | sed "s/,/ /g")  
     do
-        kill -9 $i > /dev/null 2>&1
+        kill -9 $i
         echo "killing process (ID=$i) with port 8080"
         echo "==============================="
     done
